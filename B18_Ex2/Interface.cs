2df60691133e76bf5edf714e9d2925a609cabc90
galdil <galdil@gmail.com>
2018-04-28 23:54:48 +0300

    test

---
+++ B18_Ex2/Interface.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex2
+{
+	class Interface
+	{
+		string firstPlayerName;
+		public void start()
+		{
+			//do getter and setter for name 
+			//setter and getter for 
+		}
+
+		private void GetName()
+		{
+			Console.WriteLine("Hey, what's your name?");
+			string name = Console.ReadLine();
+		}
+
+
+	}
+}
+
---
+++ B18_Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B18_Ex2
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("hey");
+		}
+	}
+}
